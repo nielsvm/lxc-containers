6466628bd42bd9cba778dabbec8e056c9cfd6678
Niels van Mourik <niels.vanmourik@acquia.com>
2014-10-13 10:24:16 +0200

    Moving css into /css, for organizational purposes.

--- www/error.log/tail.php
+++ www/error.log/tail.php
@@ -93,8 +93,8 @@ require "/etc/lxc-containervars.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <title><?=basename(LOGFILE);?></title>
-    <link rel="stylesheet" type="text/css" href="/min.css">
-    <link rel="stylesheet" type="text/css" href="/icons.css">
+    <link rel="stylesheet" type="text/css" href="/css/min.css">
+    <link rel="stylesheet" type="text/css" href="/css/icons.css">
     <style>
       body {
         background-color: black;
